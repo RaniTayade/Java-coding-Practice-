4cfb9dc845c30958cbf956a8083409a120885e94
RaniTayade <ranitayade718@gmail.com>
2025-08-31 19:18:07 +0530

    Creating Add method with a and b parameters

---
+++ src/Practice.java
@@ -0,0 +1,14 @@
+public class Practice {
+
+    static void  Add() {
+        int a=4;
+        int b=2;
+        int c=a+b;
+        System.out.println(c);
+
+     }
+    public static void main(String[] args) {
+        System.out.println("Add Method");
+        Practice.Add();
+}
+}
