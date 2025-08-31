dbfed82545be554fcb68d787809b02668756eaa5
RaniTayade <ranitayade718@gmail.com>
2025-08-31 18:42:04 +0530

    Creating Add method with a and b parameters

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
+     static void Add(int a, int b) {
+         System.out.println(a + b);
+     }
     public static void main(String[] args) {
         System.out.println("Hello world!");
-    }
+        Main r=new Main();
+        Add(5,4);
+}
 }
